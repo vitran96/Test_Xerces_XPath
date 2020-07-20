f6d2a957cfc80024bd8d330576e9803cb8482abb
Chris8018 <tranuytrieuvi@protonmail.com>
2020-07-20 10:15:09 +0700

    1st commit

---
+++ TestXercesXpathFeatures/TestXercesXpathFeatures.cpp
@@ -0,0 +1,12 @@
+﻿// TestXercesXpathFeatures.cpp : Defines the entry point for the application.
+//
+
+#include "TestXercesXpathFeatures.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ TestXercesXpathFeatures/TestXercesXpathFeatures.h
@@ -0,0 +1,8 @@
+﻿// TestXercesXpathFeatures.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
